cbafe6d0dca3d62e687eb56c0c0196fa61fdaaca
Raveesh Bhalla <raveeshbhalla90@gmail.com>
2015-04-16 01:42:22 +0530

    done

--- hermes/src/main/java/in/raveesh/hermes/receivers/HermesBroadcastReceiver.java
+++ hermes/src/main/java/in/raveesh/hermes/receivers/HermesBroadcastReceiver.java
@@ -1,13 +1,13 @@
 package in.raveesh.hermes.receivers;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
 
 import in.raveesh.hermes.Hermes;
 
-public class HermesBroadcastReceiver extends BroadcastReceiver {
+public class HermesBroadcastReceiver extends WakefulBroadcastReceiver {
 
     public HermesBroadcastReceiver() {
     }
@@ -32,7 +32,6 @@ public class HermesBroadcastReceiver extends BroadcastReceiver {
             ExponentialBackoffReceiver.attemptRegistration(context, Hermes.getDelay(), Hermes.getSenderId());
         } else {
             Log.d(Hermes.TAG, "GCM Message Received");
-
         }
     }
 }
